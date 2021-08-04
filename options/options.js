596b5c186296169f456e70ae7204da333c855b71
Crowdbotics <team@crowdbotics.com>
2021-08-04 02:37:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "spring_mud_29438Identifier",
+  url: "https://spring_mud_29438Identifier.botics.co",
+  api: "https://spring_mud_29438Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
